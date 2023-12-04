5275e5dc0ed9e2b3d7d830d655dc10551d251f3e
josgood4 <jwosgood4@gmail.com>
2023-12-03 21:34:28 -0500

    got task 2 working

---
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -0,0 +1,10 @@
+package com.mindex.challenge.dao;
+
+import com.mindex.challenge.data.Compensation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface CompensationRepository extends MongoRepository<Compensation, String> {
+    @Query("{ 'employee.employeeId' : ?0 }")
+    Compensation findCompensationByEmployeeEmployeeId(String employeeId);
+}
---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,33 @@
+package com.mindex.challenge.data;
+
+import java.time.LocalDate;
+
+public class Compensation {
+    Employee employee;
+    int salary;
+    LocalDate effectiveDate;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
