b945364b76eed9a0d713b76cd30b6788a35d8d42
josgood4 <jwosgood4@gmail.com>
2023-12-03 20:35:51 -0500

    adding interface for ReportingStructureService

--- src/main/java/com/mindex/challenge/service/ReportingStructureService.java
+++ src/main/java/com/mindex/challenge/service/ReportingStructureService.java
@@ -1,52 +1,7 @@
 package com.mindex.challenge.service;
 
-import com.mindex.challenge.dao.EmployeeRepository;
-import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
-public class ReportingStructureService {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureService.class);
-
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
-    // TODO docs
-    // TODO interface?
-    public ReportingStructure read(String id) {
-        LOG.debug("Getting ReportingStructure for employee with id [{}]", id);
-
-        Employee employee = employeeRepository.findByEmployeeId(id);
-        if (employee == null) {
-            throw new RuntimeException("Invalid employeeId: " + id);
-        }
-
-        ReportingStructure reportingStructure = new ReportingStructure(employee, getNumberOfReports(id));
-
-        return reportingStructure;
-    }
-
-    /*
-         //TODO JAVA DOCS AND FORMAT THEM
-     */
-    private int getNumberOfReports(String employeeId) {
-        Employee employee = employeeRepository.findByEmployeeId(employeeId);//LOOKUP EMPLOYEE BY ID
-        List<Employee> directReports = employee.getDirectReports();
-        int returnValue = 0;
-        if (directReports != null && !directReports.isEmpty()) {
-            // Assuming no circular directReports
-            for (Employee e : directReports) {
-                returnValue++;
-                returnValue += getNumberOfReports(e.getEmployeeId());
-            }
-        }
-        return returnValue;
-    }
+public interface ReportingStructureService {
+    ReportingStructure read(String id);
 }
---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,54 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // TODO docs
+    // TODO interface?
+    @Override
+    public ReportingStructure read(String id) {
+        LOG.debug("Getting ReportingStructure for employee with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        ReportingStructure reportingStructure = new ReportingStructure(employee, getNumberOfReports(id));
+
+        return reportingStructure;
+    }
+
+    /*
+         //TODO JAVA DOCS AND FORMAT THEM
+     */
+    private int getNumberOfReports(String employeeId) {
+        Employee employee = employeeRepository.findByEmployeeId(employeeId);//LOOKUP EMPLOYEE BY ID
+        List<Employee> directReports = employee.getDirectReports();
+        int returnValue = 0;
+        if (directReports != null && !directReports.isEmpty()) {
+            // Assuming no circular directReports
+            for (Employee e : directReports) {
+                returnValue++;
+                returnValue += getNumberOfReports(e.getEmployeeId());
+            }
+        }
+        return returnValue;
+    }
+}
