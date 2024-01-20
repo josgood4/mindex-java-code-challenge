9e7b9705c1ca01b7b844783171ed6baacf08292e
josgood4 <jwosgood4@gmail.com>
2024-01-20 14:27:18 -0500

    report an up-to-date employee object when creating or accessing the compensation objects

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.service.CompensationService;
 import org.slf4j.Logger;
@@ -15,10 +17,14 @@ public class CompensationServiceImpl implements CompensationService {
     @Autowired
     private CompensationRepository compensationRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Compensation create(Compensation compensation) {
         LOG.debug("Creating compensation [{}]", compensation);
 
+        setEmployeeDataFromRepository(compensation);
         compensationRepository.insert(compensation);
 
         return compensation;
@@ -26,14 +32,33 @@ public class CompensationServiceImpl implements CompensationService {
 
     @Override
     public Compensation read(String employeeId) {
-        LOG.debug("Getting Compensation with employeeId [{}]", employeeId);
+        LOG.debug("Getting Compensation with employeeId [{}]. ", employeeId);
 
         Compensation compensation = compensationRepository.findCompensationByEmployeeEmployeeId(employeeId);
 
         if (compensation == null) {
             throw new RuntimeException("No compensations found for employee id: " + employeeId);
         }
+        // If there are updates to employee data, they will not be persisted in
+        // the compensation object here - only reported back from the endpoint
+        setEmployeeDataFromRepository(compensation);
 
         return compensation;
+    }
+
+    private void setEmployeeDataFromRepository(Compensation compensation) {
+        if (compensation.getEmployee() == null || compensation.getEmployee().getEmployeeId() == null) {
+            throw new RuntimeException("Employee or employee id not provided for Compensation.");
+        }
+
+        // If there are differences between employee data in the repository
+        // and the employee data already present in the compensation,
+        // the repository data is assumed to be the most up-to-date.
+        String employeeId = compensation.getEmployee().getEmployeeId();
+        Employee employee = employeeRepository.findByEmployeeId(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee not found in the repository: " + employeeId);
+        }
+        compensation.setEmployee(employee);
     }
 }
